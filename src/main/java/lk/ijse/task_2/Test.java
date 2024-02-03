5f7079f32117f4b7a5ad209e0da801742b6002e1
AmilSrinath <amilsrinath5@gmail.com>
2024-02-03 10:58:25 +0530

    Commit_4

--- src/main/java/lk/ijse/task_2/Test.java
+++ src/main/java/lk/ijse/task_2/Test.java
@@ -10,11 +10,18 @@ import java.io.IOException;
 public class Test extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String header = req.getHeader("Customer-Header");
-
         String name = req.getParameter("name");
         String age = req.getParameter("age");
         System.out.println(name);
         System.out.println(age);
+
+        if(req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }else {
+            Jsonb jsonb = JsonbBuilder.create();
+            var itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+            var dbProcess = new DBProcess();
+            dbProcess.saveItemOne(itemDTO,connection);
+        }
     }
 }
