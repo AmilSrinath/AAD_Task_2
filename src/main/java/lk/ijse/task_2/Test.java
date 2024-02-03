81a81e001e72857105287c0641ef3f17b77cef13
AmilSrinath <amilsrinath5@gmail.com>
2024-02-03 10:52:50 +0530

    Commit_3

--- src/main/java/lk/ijse/task_2/Test.java
+++ src/main/java/lk/ijse/task_2/Test.java
@@ -11,6 +11,10 @@ public class Test extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String header = req.getHeader("Customer-Header");
-        System.out.println(header);
+
+        String name = req.getParameter("name");
+        String age = req.getParameter("age");
+        System.out.println(name);
+        System.out.println(age);
     }
 }
